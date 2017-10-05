9f343daf20d1d0458cb4caff4f4b4be05a41728e
ssolovev <me@sergeysolovev.com>
2017-10-06 02:38:28 +0800

    Initial commit

---
+++ webpack.dev.js
@@ -0,0 +1,125 @@
+﻿const path = require('path');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const publicPath = process.env.PUBLIC_PATH || '/';
+const publicUrl = publicPath.slice(0, -1);
+
+const config = {
+  bail: true,
+  devtool: 'cheap-module-source-map',
+  entry: {
+    index: path.resolve(__dirname, 'src/index')
+  },
+  output: {
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    publicPath
+  },
+  devServer: {
+    port: 8080,
+    contentBase: false,
+    compress: true,
+    quiet: false,
+    inline: true,
+    lazy: false,
+    hot: true,
+    host: "0.0.0.0"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                paths: [
+                  path.resolve(__dirname, 'node_modules')
+                ]
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'),
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9',
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            }
+          ],
+          publicPath
+        })
+      },
+      {
+        test: /\.(ico|png|svg|woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'static/media/[name].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new InterpolateHtmlPlugin({
+      PUBLIC_URL: publicUrl
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development'),
+        'process.env.PUBLIC_PATH': JSON.stringify(publicPath)
+      }
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'public/index.html'
+    }),
+    new CopyWebpackPlugin([
+        {from: 'public/favicons'},
+        {from: 'public'}
+      ],
+      {
+        ignore: 'public/index.html'
+      }
+    ),
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true
+    }),
+    new ManifestPlugin({
+        fileName: 'asset-manifest.json'
+    })
+  ]
+};
+
+module.exports = config;
---
+++ webpack.prod.js
@@ -0,0 +1,143 @@
+﻿const path = require('path');
+const autoprefixer = require('autoprefixer');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const InterpolateHtmlPlugin = require('interpolate-html-plugin');
+
+const publicPath = process.env.PUBLIC_PATH || '/';
+const publicUrl = publicPath.slice(0, -1);
+const publicDir = 'dist';
+const outputDir = path.resolve(__dirname, publicDir);
+
+const config = {
+  bail: true,
+  devtool: 'source-map',
+  entry: {
+    index: path.resolve(__dirname, 'src/index')
+  },
+  output: {
+    filename: 'static/js/[name].[chunkhash:8].js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    path: outputDir,
+    publicPath
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(css|less)$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader'
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                paths: [
+                  path.resolve(__dirname, 'node_modules'),
+                  path.resolve(__dirname, 'app')
+                ]
+              }
+            },
+            {
+              loader: 'postcss-loader',
+              options: {
+                ident: 'postcss',
+                plugins: () => [
+                  require('postcss-flexbugs-fixes'),
+                  autoprefixer({
+                    browsers: [
+                      '>1%',
+                      'last 4 versions',
+                      'Firefox ESR',
+                      'not ie < 9',
+                    ],
+                    flexbox: 'no-2009',
+                  }),
+                ],
+              },
+            }
+          ],
+          publicPath
+        })
+      },
+      {
+        test: /\.(ico|png|svg|woff|woff2|eot|ttf|otf)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: 'static/media/[name].[hash:8].[ext]'
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin([
+      outputDir
+    ]),
+    new InterpolateHtmlPlugin({
+      PUBLIC_URL: publicUrl
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production'),
+        'process.env.PUBLIC_PATH': JSON.stringify(publicPath)
+      }
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'public/index.html',
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      }
+    }),
+    new CopyWebpackPlugin([
+        {from: 'public/favicons'},
+        {from: 'public'}
+      ],
+      {
+        ignore: 'public/index.html'
+      }
+    ),
+    new webpack.HashedModuleIdsPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new UglifyJSPlugin({
+      sourceMap: true
+    }),
+    new ExtractTextPlugin({
+      filename: 'static/css/[name].[contenthash:8].css',
+      allChunks: true
+    }),
+    new ManifestPlugin({
+        fileName: 'asset-manifest.json'
+    })
+  ]
+};
+
+module.exports = config;
